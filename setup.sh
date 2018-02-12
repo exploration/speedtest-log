167933c0828c99deaef7a091e5c8a4133565f350
Donald Merand <dmerand@explo.org>
2018-02-12 13:18:09 -0500

    initial commit. things work

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+set -eu
+
+chmod +x speedtest-log.rb
+
+if [ ! -e 'bin/speedtest-cli' ]; then
+  printf "\n%s\n" "downloading speedtest-cli..."
+  wget -O bin/speedtest-cli https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py
+  chmod +x bin/speedtest-cli
+fi
+
+if [ ! -e 'bin/hipchat' ]; then
+  printf "\n%s\n" "downloading hipchat utility..."
+  wget -O bin/hipchat https://raw.githubusercontent.com/dmerand/dlm-dot-bin/master/hipchat
+  chmod +x bin/hipchat
+  printf "%s\n" "NOTE: you'll want to edit bin/hipchat to set up API values"
+fi
+
+if [ ! -d log ]; then
+  mkdir log
+fi
+if [ ! -e 'log/speedtest.log' ]; then
+  bin/speedtest-cli --csv-header > 'log/speedtest.log'
+  printf "\n%s\n" "log/speedtest.log created"
+fi
---
+++ speedtest-log.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'CSV'
+
+def dir
+  File.dirname(__FILE__)
+end
+
+LOGFILE = "#{dir}/log/speedtest.log"
+DOWNLOAD_MINIMUM_MB = 40
+UPLOAD_MINIMUM_MB = 20
+NOTIFY_USERNAMES = %w[Eric Donald].freeze
+
+results = `#{dir}/bin/speedtest-cli --csv`
+File.open(LOGFILE, 'a') { |f| f.puts results }
+
+def check_minimum(variety, raw_value, threshold)
+  users = NOTIFY_USERNAMES.map { |r| "@#{r}" }.join(' ')
+
+  mb_value = raw_value.to_f / 1_000_000
+  value = mb_value.truncate 2
+
+  msg = "Warning - #{variety} speed of #{value}Mb/sec is under the " \
+        "minimum threshold of #{threshold}."
+
+  puts msg if value < threshold
+  `#{dir}/bin/hipchat "#{users} #{msg}"` if value < threshold
+end
+
+# CSV format is:
+# Server ID,Sponsor,Server Name,
+#   Timestamp,Distance,Ping,Download,Upload,Share,IP Address
+values = CSV.parse(results)[0]
+
+check_minimum 'download', values[6], DOWNLOAD_MINIMUM_MB
+check_minimum 'upload', values[7], UPLOAD_MINIMUM_MB
