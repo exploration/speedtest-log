39814691e7f4b067914ed041f17a2eb9d515df51
Donald Merand <dmerand@explo.org>
2018-02-14 20:47:41 -0500

    add multiple attempts

--- speedtest-log.rb
+++ speedtest-log.rb
@@ -9,29 +9,39 @@ end
 
 DOWNLOAD_MINIMUM_MB = 40
 UPLOAD_MINIMUM_MB = 20
+
+# How many speed test attempts should we be averaging together?
+ATTEMPTS = 2
 LOGFILE = "#{dir}/log/speedtest.log"
+# These are chat service short-handles (eg HipChat)
 NOTIFY_USERNAMES = %w[Eric Donald].freeze
 
-results = `#{dir}/bin/speedtest-cli --csv`
-File.open(LOGFILE, 'a') { |f| f.puts results }
-
-def check_minimum(variety, raw_value, threshold)
-  users = NOTIFY_USERNAMES.map { |r| "@#{r}" }.join(' ')
-
+def mb_value(raw_value)
   mb_value = raw_value.to_f / 1_000_000
-  value = (mb_value * 100).floor / 100.0
+  (mb_value * 100).floor / 100.0
+end
+
+def check_minimum(variety, results, index, threshold)
+  values = results.map { |a| mb_value(CSV.parse(a)[0][index]) }
+  value = values.reduce(:+) / ATTEMPTS
 
   msg = "Warning - #{variety} speed of #{value}Mb/sec is under the " \
         "minimum threshold of #{threshold}."
-
   puts msg if value < threshold
+
+  users = NOTIFY_USERNAMES.map { |r| "@#{r}" }.join(' ')
   `#{dir}/bin/hipchat "#{users} #{msg}"` if value < threshold
 end
+
+# ---- ACTUAL WORK HERE ---- #
+
+# Note that we wait 30 seconds between each test
+results =
+  Array.new(ATTEMPTS).map { `#{dir}/bin/speedtest-cli --csv && sleep 30` }
+results.each { |r| File.open(LOGFILE, 'a') { |f| f.puts r } }
 
 # CSV format is:
 # Server ID,Sponsor,Server Name,
 #   Timestamp,Distance,Ping,Download,Upload,Share,IP Address
-values = CSV.parse(results)[0]
-
-check_minimum 'download', values[6], DOWNLOAD_MINIMUM_MB
-check_minimum 'upload', values[7], UPLOAD_MINIMUM_MB
+check_minimum 'download', results, 6, DOWNLOAD_MINIMUM_MB
+check_minimum 'upload', results, 7, UPLOAD_MINIMUM_MB
